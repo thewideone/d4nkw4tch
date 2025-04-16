006627e92388c8655e13b6791ef9c7d6001a5de8
thewideone <szymon.kajda@onet.pl>
2025-04-16 20:43:59 +0200

    Forgot to add scenes in the last commit

---
+++ source/3d/scenes/scene1.h
@@ -0,0 +1,29 @@
+#ifndef _SCENE1_H_
+#define _SCENE1_H_
+
+// 
+// Generated for lib3d by scene descriptor generator by Szymon Kajda.
+// Scene name: scene1
+// Fixed point type: int32_t
+// Fixed point binary digits: 16
+// 
+
+#include "lib3d_scene.h"
+
+#define SCENE1_OBJ_CUBE_TRI_INSTANCE_COUNT 1
+
+// Object instances ID's
+#define SCENE1_OBJ_CUBE_TRI_I0_ID 0
+
+// Number of different meshes in the scene
+#define SCENE1_MESH_COUNT 1
+// Total number of objects in the scene (different meshes * their no. of instances)
+#define SCENE1_OBJ_COUNT 1
+// Total number of cameras in the scene
+#define SCENE1_CAM_COUNT 1
+
+extern l3d_scene_t scene1;
+
+l3d_err_t scene1_init(void);
+
+#endif // _SCENE1_H_
